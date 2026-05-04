fd1085484c960f700d15cd66d2c81bb1c82fe23c
Digyfreelance <digifreelance2025@gmail.com>
2026-05-04 13:34:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/tucas-modern-hub/",
+  base: "/grmelitewear/",
   server: {
     host: "::",
     port: 8080,
